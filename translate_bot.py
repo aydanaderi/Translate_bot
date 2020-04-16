540ea37107c927ee81f105318ba4807b5ba037ee
ayda@ayda <ayda.f.naderi@gmail.com>
2020-04-16 17:13:55 +0430

    edit translate bot

--- translate_bot.py
+++ translate_bot.py
@@ -1,33 +1,29 @@
-from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+from langdetect import detect
 from mtranslate import translate
 import telepot
 import time
 
-bot = telepot.Bot('Token')
-
+bot = telepot.Bot('TOKEN')
 def handle(msg):
     content_type,chat_type,chat_id = telepot.glance(msg)
     if content_type == 'text':
         text = msg['text']
-        if text == '/start' :
+        if text == '/start':
             bot.sendMessage(chat_id,'hi! welcom to translate bot! you can send /help to understand how to use this bot')
-        if text == '/help':
-           keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = "English"), KeyboardButton(text = "Persion")]])
-           bot.sendMessage(chat_id, 'select your string language :', reply_markup = keyboard)
-    else :
-        bot.sendMessage(chat_id,'your content type is wrong!')
-def main(msg):
-    chat_id = telepot.glance(msg)
-    bot.sendMessage(chat_id, 'please send your string :')
-    text = msg['text']
-    if text == 'English':
-        translated = translate(text, 'fa', 'en')
-        bot.sendMessage(chat_id, translated)
-    if text == 'Persion':
-        translated = translate(text, 'en', 'fa')
-        bot.sendMessage(chat_id, translated)
-
+        elif text == '/help':
+            bot.sendMessage(chat_id,'send your string for translating :')
+        else :
+            if detect(text) == 'en' :
+               translated = translate(text,'fa','en')
+               bot.sendMessage(chat_id, translated)
+            elif detect(text) == 'fa' :
+               translated = translate(text, 'en', 'fa')
+               bot.sendMessage(chat_id, translated)
+            else :
+                bot.sendMessage(chat_id,'that is wrong!')
+    else:
+         bot.sendMessage(chat_id, 'your content type is wrong!')
 bot.message_loop(handle)
 print ('Listening ...')
 while 1:
-    time.sleep(20)
+    time.sleep(30)
